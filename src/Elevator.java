572dcfbea75fcccc55918edf9de1d69e04eddb87
Kirsti Norton <kirsti.norton@domo.com>
2018-07-21 21:22:48 -0600

    Elevator progress

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Elevator {
+
+    static public final int MAX_TRIPS = 100;
 
     int currentFloor = 0;
     int direction = 0; //up -1, idle = 0, up = 1
     boolean maintenanceMode = false;
     int totalTrips = 0;
     int totalFloorsPassed = 0;
+    List<Integer> requests = new ArrayList<>();
 
     public Elevator() {
 
@@ -20,5 +26,39 @@ public class Elevator {
         System.out.println("Moving Down");
         currentFloor--;
         totalFloorsPassed++;
+    }
+
+    public void openDoors() {
+        System.out.println("Opening Door on floor" + currentFloor);
+    }
+
+    public void closeDoors() {
+        System.out.println("Closing Door on floor" + currentFloor);
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void addRequest(int request) {
+        requests.add(request);
+    }
+
+    public boolean isMaintenanceMode() {
+        return maintenanceMode;
+    }
+
+    public void service() {
+        maintenanceMode = false;
+    }
+
+    private void checkMaintenance() {
+        if (totalTrips > MAX_TRIPS) {
+            maintenanceMode = true;
+        }
     }
 }
--- src/ElevatorController.java
+++ src/ElevatorController.java
@@ -2,11 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ElevatorController {
+    int minFloor = 1;
+    int maxFloor;
     int totalFloors;
     List<Elevator> elevators;
 
-    public ElevatorController(int totalFloors, int totalElevators) {
-        this.totalFloors = totalFloors;
+    public ElevatorController(int maxFloor, int totalElevators) {
+        this.maxFloor = maxFloor;
         this.elevators = new ArrayList<>(totalElevators);
         for (int i = 0; i < totalElevators; i++) {
             elevators.add(new Elevator());
